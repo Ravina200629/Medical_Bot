d3dd43c8315a010e9948f4903e69d540885f037a
siddharthan <75602882+siddharthan051996@users.noreply.github.com>
2026-07-09 11:44:40 +0530

    all projects updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Ravina R",
+    author_email="rameshkumarravina@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
