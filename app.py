219b72b48a641fb0b888f2be0da949b9ac8d7681
Ravina200629 <rameshkumarravina@gmail.com>
2026-07-15 18:24:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,5 +67,7 @@ def chat():
 
 
 
-if _name_ == '_main_':
-    app.run(host="0.0.0.0", port= 7860, debug= True)
+if __name__ == "__main__":
+
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=False)
